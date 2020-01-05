a6da6f9a506b6ca5eb7dbe2928d36ba1b2c9d400
eleonard1224 <45740519+eleonard1224@users.noreply.github.com>
2020-01-05 17:09:28 -0500

    Add files via upload

--- FloatingPointNumbers/floatpt.py
+++ FloatingPointNumbers/floatpt.py
@@ -1,1 +1,27 @@
+'''
+Function for returning a floating point number given a base, mantissa, and exponent
+'''
 
+# Importing the libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+# Defining the floatpt method which returns a floating point number given a
+#     base, mantissa, and exponent
+# beta - base
+# m - mantissa - 0.b1 b2 ... bt
+# t - mantissa length
+# e - exponent
+def floatpt(beta,m,t,e):
+    
+    r = m * math.pow(beta,e-t);
+    return r;
+
+# Tests
+r1 = floatpt(10,123,3,2);
+print(r1);
+
+r2 = floatpt(2,123,3,2);
+print(r2);
